4678d40da46a7762e5346f46cff0b52a6285a17e
anthony <yoskovia@mail.gvsu.edu>
2020-06-11 17:51:17 -0400

    Plotting trace times

--- src/parseTrace.py
+++ src/parseTrace.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[17]:
+# In[48]:
 
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import csv
 from functools import reduce
 from datetime import datetime
 
 get_ipython().run_line_magic('run', './trace_helps.ipynb')
 
-with open('FT2.TXT') as f:
+pd.set_option('max_colwidth', 1000)
+
+with open('/home/anthony/TRACE_LOG.TXT') as f:
     reader = csv.reader(f, delimiter='\n')
     dat = []
     a = []
@@ -27,8 +31,16 @@ for i, x in enumerate(dat):
     if is_norm(x):
         al.append(clean_dat(x, i))
 
-full = pd.concat(al)
-full
+        
+full = pd.concat(al).explode('times')
+full['times'] = full['times'].astype(float)
+
+
+FRONT = ['trace_id', 'start', 'end', 'trace_line']
+clean = full[FRONT + [x for x in full.columns if x not in FRONT]].drop(['trace_entry', 'whereto'], axis=1)
+
+ROUTE_TIMES = clean.drop_duplicates(subset=['trace_id']).apply(lambda x: x['end'] - x['start'], axis=1)
+plt.hist(ROUTE_TIMES / np.timedelta64(1, 's'));
 
 
 # In[ ]:
